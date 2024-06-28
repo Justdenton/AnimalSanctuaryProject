5402f5391b2a3dddbc894f6a8ac8315fa0bb8f8a
“Justin” <jdenton.bills@gmail.com>
2024-06-28 17:58:22 -0500

    Add Animal layout

--- src/com/skilldistillery/sanctuary/Animal.java
+++ src/com/skilldistillery/sanctuary/Animal.java
@@ -1,6 +1,35 @@
 package com.skilldistillery.sanctuary;
-
+// No instances of the Animal class are constructed, only objects of the subclass types. (GRADING)
 public class Animal {
 	
+	// Private name field.
+	private String name;
+	
+	// Constructor.
+	public Animal(String name) {
+		this.name = name; 
+	}
+	
+	// public void makeNoise method.
+	public void makeNoise() {
+		System.out.println("prints string - animal's (animal object) noise");
+	}
+	
+	// eat method with an int parameter - amount of food it is being fed.
+		// When called - says how much food it is eating.
+	public void eat(int fedFood) {
+		
+		System.out.println("I'm eating # of food");
+		
+	}
+
+	// GET & SET for name field.
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
